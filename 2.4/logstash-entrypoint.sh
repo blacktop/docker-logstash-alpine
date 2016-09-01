fd242cc34dc588a101da26db7e90a6ab8798c749
blacktop <blacktop@users.noreply.github.com>
2016-08-31 20:19:10 -0600

    add version 2.4

---
+++ 2.4/logstash-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# Add logstash as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- logstash "$@"
+fi
+
+# Run as user "logstash" if the command is "logstash"
+if [ "$1" = 'logstash' ]; then
+	chown -R logstash:logstash /usr/share/logstash
+	
+	set -- gosu logstash tini -- "$@"
+fi
+
+exec "$@"
